ab0b05f94636fe10f4140968c0986c5b27981bdb
Sri Widyaningrum <swidyaningrum5@gmail.com>
2022-03-05 21:53:14 +0700

    SRI WIDYANINGRUM F55119087

---
+++ app/src/main/java/com/tugas1mobile_srywidyaningrumf55119087/MainActivity.java
@@ -0,0 +1,18 @@
+
+
+
+
+package com.tugas1mobile_srywidyaningrumf55119087;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
